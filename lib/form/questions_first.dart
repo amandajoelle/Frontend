606fe07d5a740af0299901bca1c3fc3b3891a504
obfuscated-bytecode <confusio@ist-einmalig.de>
2021-01-03 16:27:19 +0100

    add fields to questionnaire

--- lib/form/questions_first.dart
+++ lib/form/questions_first.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+String dropdownValue = 'One';
+enum Gender { maennlich, weiblich, unbekannt }
+Gender _gender = Gender.maennlich;
+
 // Define a custom Form widget.
 class Questions_First extends StatefulWidget {
   @override
@@ -18,21 +22,183 @@ class Questions_FirstState extends State<Questions_First> {
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
+  int currentStep = 0;
+  bool complete = false;
+
+  next() {
+    currentStep + 1 != steps.length
+        ? goTo(currentStep + 1)
+        : setState(() => complete = true);
+  }
+
+  cancel() {
+    if (currentStep > 0) {
+      goTo(currentStep - 1);
+    }
+  }
+
+  goTo(int step) {
+    setState(() => currentStep = step);
+  }
+
+  List<Step> steps = [
+    Step(
+      title: const Text('Zuständiges Fachgebiet'),
+      isActive: true,
+      state: StepState.editing,
+      content: Column(
+        children: <Widget>[
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              /*setState(() {
+                dropdownValue = newValue;
+              });*/
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    ),
+    Step(
+      title: const Text('Altersgruppe des Patienten'),
+      subtitle: const Text("falls betroffen"),
+      isActive: false,
+      state: StepState.editing,
+      content: Column(
+        children: <Widget>[
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              /*setState(() {
+                dropdownValue = newValue;
+              });*/
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    ),
+    Step(
+      state: StepState.editing,
+      title: const Text('Geschlecht des Patienten'),
+      subtitle: const Text("falls betroffen"),
+      content: Column(
+        children: <Widget>[
+          ListTile(
+            title: const Text("männlich"),
+            leading: Radio(
+              value: Gender.maennlich,
+              groupValue: _gender,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("weiblich"),
+            leading: Radio(
+              value: Gender.weiblich,
+              groupValue: _gender,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("unbekannt"),
+            leading: Radio(
+              value: Gender.unbekannt,
+              groupValue: _gender,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      title: const Text('Wo ist das Ereignis passiert?'),
+      isActive: false,
+      state: StepState.editing,
+      content: Column(
+        children: <Widget>[
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              /*setState(() {
+                dropdownValue = newValue;
+              });*/
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-        key: _formKey,
-        child: Column(children: <Widget>[
-          TextFormField(
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          )
+    return new Scaffold(
+         body: Column(children: <Widget>[
+          Expanded(
+            child: Stepper(
+              steps: steps,
+              currentStep: currentStep,
+              onStepContinue: next,
+              onStepTapped: (step) => goTo(step),
+              onStepCancel: cancel,
+            ),
+          ),
         ]));
   }
 }
--- lib/form/questions_second.dart
+++ lib/form/questions_second.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
+String dropdownValue = 'One';
+
+enum Job { Pflegepersonal, Arzt, Psychotherapeut, Apotheker, andere_Berufsgruppe }
+Job _job = Job.Pflegepersonal;
+
+enum Frequency {nicht_anwendbar, taeglich, monatlich, jaehrlich, erstmalig}
+Frequency _frequency = Frequency.nicht_anwendbar;
+
 // Define a custom Form widget.
 class Questions_Second extends StatefulWidget {
   @override
@@ -18,21 +26,231 @@ class Questions_SecondState extends State<Questions_Second> {
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
+  int currentStep = 0;
+  bool complete = false;
+
+  next() {
+    currentStep + 1 != steps.length
+        ? goTo(currentStep + 1)
+        : setState(() => complete = true);
+  }
+
+  cancel() {
+    if (currentStep > 0) {
+      goTo(currentStep - 1);
+    }
+  }
+
+  goTo(int step) {
+    setState(() => currentStep = step);
+  }
+
+  List<Step> steps = [
+    Step(
+      title: const Text('Zuständiges Fachgebiet'),
+      isActive: true,
+      state: StepState.editing,
+      content: Column(
+        children: <Widget>[
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              /*setState(() {
+                dropdownValue = newValue;
+              });*/
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    ),
+    Step(
+      title: const Text('Altersgruppe des Patienten'),
+      subtitle: const Text("falls betroffen"),
+      isActive: false,
+      state: StepState.editing,
+      content: Column(
+        children: <Widget>[
+          DropdownButton<String>(
+            value: dropdownValue,
+            icon: Icon(Icons.arrow_downward),
+            iconSize: 24,
+            elevation: 16,
+            style: TextStyle(color: Colors.deepPurple),
+            underline: Container(
+              height: 2,
+              color: Colors.deepPurpleAccent,
+            ),
+            onChanged: (String newValue) {
+              /*setState(() {
+                dropdownValue = newValue;
+              });*/
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    ),
+    Step(
+      state: StepState.editing,
+      title: const Text('Wie häufig tritt dieses Ereignis ungefähr auf?'),
+      content: Column(
+        children: <Widget>[
+          ListTile(
+            title: const Text("nicht anwendbar"),
+            leading: Radio(
+              value: Frequency.nicht_anwendbar,
+              groupValue: _frequency,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("täglich"),
+            leading: Radio(
+              value: Frequency.taeglich,
+              groupValue: _frequency,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("monatlich"),
+            leading: Radio(
+              value: Frequency.monatlich,
+              groupValue: _frequency,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("jährlich"),
+            leading: Radio(
+              value: Frequency.jaehrlich,
+              groupValue: _frequency,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("erstmalig"),
+            leading: Radio(
+              value: Frequency.erstmalig,
+              groupValue: _frequency,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+        ],
+      ),
+    ),
+    Step(
+      state: StepState.editing,
+      title: const Text('Wer berichtet?'),
+      subtitle: const Text("Berufsgruppe"),
+      content: Column(
+        children: <Widget>[
+          ListTile(
+            title: const Text("Pflege-, Praxispersonal"),
+            leading: Radio(
+              value: Job.Pflegepersonal,
+              groupValue: _job,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("Arzt / Ärztin, Psychotherapeut/in"),
+            leading: Radio(
+              value: Job.Arzt,
+              groupValue: _job,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("Apotheker / Apothekerin"),
+            leading: Radio(
+              value: Job.Apotheker,
+              groupValue: _job,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+          ListTile(
+            title: const Text("andere Berufsgruppe"),
+            leading: Radio(
+              value: Job.andere_Berufsgruppe,
+              groupValue: _job,
+              /*onChanged: (Gender value) {
+                setState(() {
+                _gender = value;
+                });
+              }*/
+            ),
+          ),
+        ],
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    // Build a Form widget using the _formKey created above.
-    return Form(
-        key: _formKey,
-        child: Column(children: <Widget>[
-          TextFormField(
-            // The validator receives the text that the user has entered.
-            validator: (value) {
-              if (value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          )
+    return new Scaffold(
+        body: Column(children: <Widget>[
+          Expanded(
+            child: Stepper(
+              steps: steps,
+              currentStep: currentStep,
+              onStepContinue: next,
+              onStepTapped: (step) => goTo(step),
+              onStepCancel: cancel,
+            ),
+          ),
         ]));
   }
 }
